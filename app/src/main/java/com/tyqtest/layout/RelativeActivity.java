0f714bd681e7e2ad751f4f259e55c28dc56b8117
tyq1 <yaqing.tan@avanade.com>
2017-10-13 15:02:09 +0800

    Initial commit

---
+++ app/src/main/java/com/tyqtest/layout/RelativeActivity.java
@@ -0,0 +1,16 @@
+package com.tyqtest.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 谭雅清 on 2017/10/13.
+ */
+
+public class RelativeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative);
+    }
+}
